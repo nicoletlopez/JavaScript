8dbe583136f8f562c38c8de89f2604cd259445dd
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-10-12 14:39:43 +0800

    aw

---
+++ FreeCodeCamp/BasicJavascript/notes.js
@@ -0,0 +1,4 @@
+// ...array
+// https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/es6/use-the-spread-operator-to-evaluate-arrays-in-place
+
+//use CAPS variable name for const variables
